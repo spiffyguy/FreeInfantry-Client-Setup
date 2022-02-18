bb5ce44d40d4e7f5c21e31953d4e5a92c59c95ca
spiffyguy <matt@techdavis.com>
2022-02-18 06:25:52 -0800

    Mac Packaging cleanup.  The buildPackageDMG reads the version number from the .app file now.  Added variables to seperate things better,  cleaned up the packaging & dmg naming.

--- Mac/buildPackageDMG.sh
+++ Mac/buildPackageDMG.sh
@@ -3,56 +3,94 @@
 APP_NAME="Infantry Online"
 APP_LOCATION="./_builds/app/${APP_NAME}.app"
 
+SKIP_PKG=false
+SKIP_DMG=false
+
 # cd current working directory
 WORKINGDIR=$(cd "$(dirname "$BASH_SOURCE")"; cd -P "$(dirname "$(readlink "$BASH_SOURCE" || echo .)")"; pwd)
 cd $WORKINGDIR
 
-APP_VERSION="1.55b"
-#TODO: Get this working...
-#APP_VERSION=$(defaults read "${APP_LOCATION}/Contents/Info.plist" CFBundleShortVersionString)
-
-PKG_LOCATION="./_builds/pkg/${APP_NAME} ${APP_VERSION}.pkg"
-DMG_LOCATION="./_builds/dmg/${APP_NAME//[[:blank:]]/}_${APP_VERSION}_Mac.dmg"
-
-echo ""
-echo "PACKAGING ${APP_LOCATION}, Version: ${APP_VERSION}"
-echo ""
-echo "***** STEP 1/3 - CLEAN UP OLD ${APP_VERSION} PKG & DMG BUILDS FIRST"
-echo ""
-test -f "${DMG_LOCATION}" && rm "${DMG_LOCATION}"
-test -f "${PKG_LOCATION}" && rm "${PKG_LOCATION}"
-
-echo "***** STEP 2/3 - BUILDING THE .PKG INSTALLER FILE (Will take a few minutes...)"
-echo ""
 if [ -d "${APP_LOCATION}" ]; then
-	/usr/local/bin/packagesbuild "./_assets/Infantry Online.pkgproj"
+	APP_VERSION=$(plutil -extract CFBundleShortVersionString raw -o - "${APP_LOCATION}/Contents/Info.plist")
+	#OVERRIDE PACKAGING VISIBLE VERSION?
+	#APP_VERSION="1.55b"
+	
+	APP_NAMEVERSION_NOSPACES="${APP_NAME//[[:blank:]]/}_${APP_VERSION//[[:blank:]]/}"
+	
+	PKGPROJ_LOCATION="./_assets/${APP_NAME}.pkgproj"
+	PKG_LOCATION="./_builds/pkg/${APP_NAME} ${APP_VERSION}.pkg"
+	DMG_LOCATION="./_builds/dmg/${APP_NAMEVERSION_NOSPACES}_Mac.dmg"
+	
+	echo ""
+	echo "*********"
+	echo "PACKAGING ${APP_LOCATION}, Version: ${APP_VERSION}"
+	echo "*********"
+	echo ""
+	echo "***** STEP 1/3 - CLEAN UP OLD ${APP_VERSION} PKG & DMG BUILDS FIRST"
+	echo ""
+	if [ "$SKIP_PKG" = false ]; then
+		test -f "${PKG_LOCATION}" && rm "${PKG_LOCATION}"
+	fi
+	if [ "$SKIP_DMG" = false ]; then
+		test -f "${DMG_LOCATION}" && rm "${DMG_LOCATION}"
+	else
+		if [ "$SKIP_PKG" = false ]; then
+			# if a new PKG is being created, remove the same named DMG version that will be obsolete...
+			test -f "${DMG_LOCATION}" && rm "${DMG_LOCATION}"
+		fi
+	fi
+	
+	echo "***** STEP 2/3 - BUILDING THE .PKG INSTALLER FILE (Will take a few minutes...)"
 	echo ""
 	
+	if [ "$SKIP_PKG" = false ]; then
+		# UPDATE .pkg NAMES
+		/usr/bin/plutil -replace PROJECT.PROJECT_SETTINGS.NAME -string "${APP_NAME} ${APP_VERSION}" "${PKGPROJ_LOCATION}"
+		/usr/bin/plutil -replace PROJECT.PACKAGE_SETTINGS.VERSION -string "${APP_VERSION}" "${PKGPROJ_LOCATION}"
+		/usr/bin/plutil -replace PROJECT.PACKAGE_SETTINGS.IDENTIFIER -string "com.${USER}.${APP_NAMEVERSION_NOSPACES}" "${PKGPROJ_LOCATION}"
+		
+		# Notice extra dot before APP_LOCATION to mean back a directory...
+		/usr/bin/plutil -replace PROJECT.PACKAGE_FILES.HIERARCHY.CHILDREN.0.CHILDREN.0.PATH -string ".${APP_LOCATION}" "${PKGPROJ_LOCATION}"
+		
+		/usr/bin/sed -i '' -e 's/\(file "\)\(.*\)\("\)/\1'"$(printf '%s\n' "/Applications/${APP_NAME}.app" | sed -e 's/[\/&]/\\&/g')"'\"/g' ./_assets/scripts/reveal.sh
+	
+		/usr/local/bin/packagesbuild "${PKGPROJ_LOCATION}"
+	else
+		echo "SKIPPING PKG CREATION"
+	fi
+	echo ""
+		
 	echo "***** STEP 3/3 - BUILDING THE DMG (Will take a few minutes...)"
 	echo ""
 	if [ -f "${PKG_LOCATION}" ]; then
-		create-dmg \
-		--volname "Install ${APP_NAME} ${APP_VERSION}" \
-		--volicon "./_assets/images/floppy.icns" \
-		--background "./_assets/images/background.png" \
-		--window-pos 200 120 \
-		--window-size 512 410 \
-		--icon-size 96 \
-		--text-size 12 \
-		--no-internet-enable \
-		--icon "${APP_NAME} ${APP_VERSION}.pkg" 385 240 \
-		"${DMG_LOCATION}" \
-		"./_builds/pkg/"
+		if [ "$SKIP_DMG" = false ]; then
+			create-dmg \
+			--volname "Install ${APP_NAME} ${APP_VERSION}" \
+			--volicon "./_assets/images/floppy.icns" \
+			--background "./_assets/images/background.png" \
+			--window-pos 200 120 \
+			--window-size 512 410 \
+			--icon-size 96 \
+			--text-size 12 \
+			--no-internet-enable \
+			--icon "${APP_NAME} ${APP_VERSION}.pkg" 385 230 \
+			"${DMG_LOCATION}" \
+			"./_builds/pkg/"
+			
+			if [ -f "${DMG_LOCATION}" ]; then
+				echo "DMG built here:"
+				echo "${DMG_LOCATION}"
+				open ./_builds/dmg/
+			else
+				echo "ERROR: ${DMG_LOCATION} is missing"
+				echo ""
+			fi
+			
+		else
+			echo "SKIPPING DMG CREATION"
+		fi
 		echo ""
 		
-		if [ -f "${DMG_LOCATION}" ]; then
-			echo "DMG built here:"
-			echo "${DMG_LOCATION}"
-			open ./_builds/dmg/
-		else
-			echo "ERROR: ${DMG_LOCATION} is missing"
-			echo ""
-		fi
 	else
 		echo ""
 		echo "ERROR: ${PKG_LOCATION} is missing"
--- README.md
+++ README.md
@@ -1,59 +1,104 @@
 # Infantry Online Client Setup/Installer Files
 
-**Download the latest built, ready to run, clients at: [http://freeinfantry.com](http://freeinfantry.com)**
+**Download the latest built, ready to play, Infantry Online client at: [http://freeinfantry.com](http://freeinfantry.com)**
 
 Setup/Installer source files for the Infantry Online game:
 
 ## Windows
-
-> ### Prerequisites
->
-> - Windows 7 SP1 or later
+**Windows 7 SP1 or later**
 
 <details>
-  <summary>(TODO) Building the Installer</summary>
+  <summary>(TODO) Building the "Advanced Installer"</summary>
 
+>
 > **Download/Install First**
 > 
 > - Visual Studio 2019 Community Edition ([https://visualstudio.microsoft.com/vs/older-downloads/](https://visualstudio.microsoft.com/vs/older-downloads/))
+> 
 
-- (TODO) More things...
+1. Get the official Infantry Online Launcher .exe file either from:
+
+	- Compiling yourself from the Launcher repo or
+	- Downloading the windows launcher exe
+
+2. (TODO) More things...
+
 </details>
 
-## macOS
+<details>
+  <summary>(TODO) Installing the Game (with Screenshots)</summary>
 
-> ### Prerequisites
->
-> - macOS 10.13 (High Sierra) or later
+- TODO
+</details>
+
+
+## macOS
+**macOS 10.13 (High Sierra) or later**
 
 <details>
   <summary>(TODO) How to wrap the Windows .exe into a Mac .app with WineSkin</summary>
 
+>
 > **Download/Install First**
 > 
 > - WineSkin Wrapper ([https://github.com/Gcenx/WineskinServer](https://github.com/Gcenx/WineskinServer))
 > - cnc-ddraw ([https://github.com/CnCNet/cnc-ddraw](https://github.com/CnCNet/cnc-ddraw))
+> 
 
 - TODO
 </details>
 
 <details>
-  <summary>Building the Installer</summary>
+  <summary>Building the PKG Installer into a DMG</summary>
 
+>
 > **Download/Install First**
 > 
 > - Mac "Packages" app ([http://s.sudre.free.fr/Software/Packages/about.html](http://s.sudre.free.fr/Software/Packages/about.html))
 > - `brew install create-dmg` ([https://github.com/create-dmg/create-dmg](https://github.com/create-dmg/create-dmg))
+> 
 
  1. Place the built WineSkin wrapped client app here: "./Mac/_build/app/Infantry Online.app"
  2. Run the "./Mac/buildPackageDMG.sh" in the terminal to build a .pkg installer file and a distributable dmg file.
 </details>
 
 <details>
-  <summary>Screenshots</summary>
+  <summary>Installing the Game (with Screenshots)</summary>
+
+ - 1. Open the **DMG Volume Icon**
 
 ![DMG Volume](Mac/_screenshots/DMGVolume.png)
+
+ - 2. Open the **.pkg** file by Control-Click and choosing "Open" in the contextual menu
+
 ![DMG Installer Window](Mac/_screenshots/DMGInstallerWindow.png)
+</details>
+
+## GNU/Linux
+**Any Distro that WINE supports**
+
+<details>
+  <summary>(TODO) Lutris Script</summary>
+
+- TODO
+</details>
+
+<details>
+  <summary>(TODO) PlayOnLinux Script</summary>
+
+- TODO
+</details>
+
+<details>
+  <summary>(TODO) Building SnapCraft Script</summary>
+
+- TODO
+</details>
+
+<details>
+  <summary>(TODO) Manual Install with PlayOnLinux (with Screenshots)</summary>
+
+- TODO
 </details>
 
 ## Thanks to all Contributors
@@ -61,10 +106,11 @@ Setup/Installer source files for the Infantry Online game:
 #### FreeInfantry
  - Realm (AKA Col. Kitty Hawk) for RnD/testing
  - Jovan for helping with the repo & website
- - The whole FreeInfantry team.
+ - SocketMix for PlayOnLinux manual install guide and testing
+ - The whole FreeInfantry team and players
 
 #### CNC-DDraw
- - FunkyFr3sh for his work on cnc-ddraw to support this game
+ - FunkyFr3sh for his work on cnc-ddraw to support this game for modern windows and WINE
 
 #### Wineskin
  - Gcenx for his updates on WineSkin (originally created by doh123)
